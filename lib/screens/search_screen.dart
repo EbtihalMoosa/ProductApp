5cb845edcd27545c6fe5fb3466d8980f24cb6747
EbtihalMoosa <138448703+EbtihalMoosa@users.noreply.github.com>
2023-09-24 21:43:42 -0700

    Initial commit

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 209, 191, 240),
+      appBar: AppBar(
+        title: Text("Search Screen"),
+      ),
+      body: Column(
+        children: [
+          Container(
+              margin: EdgeInsets.all(30),
+              child: Text("Search",
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.deepPurple,
+                      fontWeight: FontWeight.bold))),
+          Container(
+            margin: EdgeInsets.all(20),
+            child: TextFormField(
+              validator: (value) {},
+              style: TextStyle(color: Colors.deepPurple),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color.fromARGB(61, 255, 255, 255),
+                hintText: "search for product",
+                prefixIcon: Icon(Icons.search, color: Colors.purple),
+                hintStyle: TextStyle(
+                  color: Colors.purple,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(width: 2, color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
